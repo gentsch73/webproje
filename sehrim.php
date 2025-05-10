8314c4d7ec557365e2f561f32e7e3ccca66f9b30
Berkay Genç <berkaygench@gmail.com>
2025-05-10 18:37:20 +0300

    windows

--- sehrim.php
+++ sehrim.php
@@ -14,13 +14,82 @@
   rel="stylesheet"
   />
 
-  <link rel="stylesheet" href="assets/css/styles.css">
+  <link rel="stylesheet" href="styles.css">
 
   <title>Ana Sayfa</title>
 </head>
 <body>
-
   <?php include 'navbar.php'; ?>
+
+  <div class="sehrim-text-container">
+    
+    <p class="text-center mt-4">
+    Tarihi Ortacalar Çifte Köprü, Arhavi’de, Mençuna Şelalesi yolu üzerinde giderken Arılı ile Küçükköy yol ayrımında karşınıza çıkan, birbirine tıpatıp benzeyen sivri kemerli iki köprü. Her ne kadar 18. yüzyıl tarihli olarak resmi kayıtlara geçmiş olsa da aslında en az bir 600 yıllık oldukları düşünülüyor. Tamamen kesme taştan yapılmış iki köprünün toplam uzunluğu yaklaşık 80 metre. Harç olarak da kireç ve yumurta akı kullanılmış. Bunca depreme, rüzgara, yağmura rağmen dimdik ayakta kalabilmesi gerçekten de üstün bir mimarinin ürünü olması. Bu arada bu köprülerin hemen yanına birer köprü yapmışız. Bu açıdan çekilince görünmüyorlar ama muhtemelen sadece bu açıdan. Oraya gittiğinizde gördüğünüz manzara kesinlikle bu fotoğraf karesi gibi güzel değil.<br><br>Karadeniz’in bizce en fantastik doğa olayı bulut denizleri. Bu muhteşem manzaraya şahit olmak için göğün 9. katındaki Sırt Yayla’ya çıktık. Sırt Yayla öyle Gito yada Pokut gibi popüler bir yer değil. Hiçbir tesis yok. Burada pofidik bulutlar yatağımız, yıldızlar yorganımız ama bulut denizinin hangi seviyede olacağı biraz da bulutların keyfine ve sizin şansınıza kalmış.
+
+  Burada kamp yapmak isteyeceksiniz ama geceleri sık sık çatı uçuran rüzgarlardan çıkıyormuş. Bizzat test ettik ve gece köylülerin evlerine sığındık.
+
+  Özellikle de gün batımında manzara enfes ama en tepeye çıkmak yerine az altındaki köylerden izlemenizi tavsiye ederiz. Bir dez zirveden bi 15 dakikalık yürüyüşle varılan ufak bir buzul gölü var. <br><br> Burası da Çifteköprü’den geçerek geleceğiniz Arhavi’deki Mençuna Şelalesi. Dik bir patikadan yarım saat yürüyerek ulaşılıyor. Çok sevindik ki şelalenin yanına kadar çıkan araç yolu, piknik alanı gibi burayı katledecek “yatırımlar” yapılmamış. Umarız bu kadar az müdahale ile kalır. Bu 30 dakikalık yürüyüşün sonunda doğal olarak oluşan havuzcukta keyif yapabilirsiniz
+  </p>
+
+  </div>
+
+
+<div class="foto-container">
+  <div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <a href="https://www.kulturportali.gov.tr/turkiye/artvin/gezilecekyer/ortacalar-cifte-kopruler">
+      <img src="https://www.kulturportali.gov.tr/repoKulturPortali/large/26022013/7cc87fe4-68e3-4910-b89c-8a5018097f13.jpg?format=jpg&quality=50" class="d-block w-100" alt="...">
+      </a>
+      <div class="carousel-caption d-none d-md-block">
+        <h5 class="img-text1">Cifte Kopru</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <a href="https://www.kulturportali.gov.tr/turkiye/artvin/gezilecekyer/cehennem-deresi-kanyonu141129">
+      <img src="https://www.kulturportali.gov.tr/contents/images/Artvin_Cehennem_deresi_kanyonu%20(4)%20kopya.jpg" class="d-block w-100" alt="...">
+      </a>
+      <div class="carousel-caption d-none d-md-block">
+        <h5 class="img-text2">Cehennem Deresi Kanyonu</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+    <a href="https://kulturportali.gov.tr/turkiye/artvin/gezilecekyer/mencuna-selalesi">
+      <img src="https://www.bizevdeyokuz.com/wp-content/uploads/mencuna-selalesi-artvin.jpg" class="d-block w-100" alt="...">
+      </a>
+      <div class="carousel-caption d-none d-md-block">
+        <h5 class="img-text3">Mencuna Selalesi</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+    <a href="http://www.kackar.org/yaylalar/sirtyaylasi.html">
+      <img src="https://www.bizevdeyokuz.com/wp-content/uploads/bulut-denizi-artvin.jpg" class="d-block w-100" alt="...">
+      </a>
+      <div class="carousel-caption d-none d-md-block">
+        <h5 class="img-text4">Sirt Yayla</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
 
 
 </body>
